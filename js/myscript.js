1c5900423fb3e16a97839801ce2d6a3c5cf8affb
readmorecomic <readmorecomic@gmail.com>
2017-05-22 13:10:59 +0900

    Add files via upload

--- js/myscript.js
+++ js/myscript.js
@@ -15,6 +15,14 @@ $(document).ready(function(){
 		$(this).css("color", 'white');
 	});
 	
+	$("#jquery").mouseover(function() {
+		$(this).css("color", "#95968e");
+	});
+	$("#jquery").mouseout(function() {
+		$(this).css("color", 'black');
+	});
+	
+
 	$(".me").click(function() {
 		audioElement.play();
 	});
